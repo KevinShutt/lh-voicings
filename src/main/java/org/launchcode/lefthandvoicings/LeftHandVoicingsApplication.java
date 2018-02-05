36781bbf413762489e0ab89a05b0a975bfdf3367
Kevin Shutt <kevinshutt@yahoo.com>
2018-02-05 13:21:58 -0600

    initial commit

---
+++ src/main/java/org/launchcode/lefthandvoicings/LeftHandVoicingsApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.lefthandvoicings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeftHandVoicingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeftHandVoicingsApplication.class, args);
+	}
+}
